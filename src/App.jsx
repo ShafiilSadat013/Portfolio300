f67f0f547714ef5388671bfa249b10ffb8709607
Shafiilsadat013 <sadatshafiil@gmail.com>
2026-09-23 19:06:58 +0600

    basic structure of navbar is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css"
+import Navbar from "./components/Navbar";
 function App()
 {
-    return (
-        <h1 id="test">This is testing</h1>
+    return(
+    <>
+     <Navbar />
+     {/* <h1>Hello</h1> */}
+    </>
     )
 
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+
+
+
+function Navbar()
+{
+    return(
+        <>
+        <div className="logo">
+            <h2>S<span className="color-orange">D</span>T</h2>
+        </div>
+        <ul className="menu">
+            <li><a href="http://">Portfolio</a></li>
+            <li><a href="http://">Blog</a></li>
+            <li><button className="btn">Hire Me</button></li>
+        </ul>
+        </>
+    )
+
+}
+export default Navbar
