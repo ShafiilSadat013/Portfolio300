42d9f795c81689bb75ac9275fe19b8e687306207
Shafiilsadat013 <sadatshafiil@gmail.com>
2026-09-23 23:51:28 +0600

    i am so sleepy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css"
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 function App()
 {
     return(
@@ -10,6 +11,8 @@ function App()
         <Navbar />
         <Hero />
      </div>
+     <About />
+
      {/* <h1>Hello</h1> */}
     </>
     )
