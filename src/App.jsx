4b77d78a511296087b516eca0b62675d571e865b
Shafiilsadat013 <sadatshafiil@gmail.com>
2026-09-24 12:10:26 +0600

    dark mode has been added to all sections

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,31 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Resume from "./components/Resume";
+import Footer from "./components/Footer";
+import { useState } from "react";
+
 function App()
 {
+    const [darkMode, setDarkMode] = useState(false);
     return(
     <>
     {/* navbar and hero different class to show the bg image properly */}
-    <div className="top">
-        <Navbar />
-        <Hero />
-     </div>
-     <About />
-     <Skills />
-     <Resume />
 
+      <div className={darkMode ? "dark-mode" : ""}> 
+            <div className="top">
+                <Navbar />
+                <Hero />
+            </div>
+            <About />
+            <Skills />
+            <Resume />
+            <Footer />
+            <button className="dark-btn" 
+            onClick={() => setDarkMode(!darkMode)}>
+                 {darkMode ? "☀️" : "🌙"}
+
+            </button>
+       </div>
      {/* <h1>Hello</h1> */}
     </>
     )
